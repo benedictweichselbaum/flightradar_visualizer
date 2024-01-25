4d27a960a9b9f2f971c7bb3610d2c56dbf1b69e5
Benedict Weichselbaum <contact@bweichselbaum.de>
2024-01-25 12:50:45 +0100

    Create first working version

--- src/app/components/flight-map/flight-map.component.ts
+++ src/app/components/flight-map/flight-map.component.ts
@@ -1,22 +1,61 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {FlightDataClientService} from "../../services/flight_data_service/flight-data-client.service";
 import {Flight} from "../../models/flight";
 import {NgForOf} from "@angular/common";
+import {LeafletModule} from "@asymmetrik/ngx-leaflet";
+import * as L from 'leaflet';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+
+const iconRetinaUrl = 'assets/marker_icon/plane_2x.png';
+const iconUrl = 'assets/marker_icon/plane.png';
+const shadowUrl = 'assets/marker_icon/plane.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 25],
+  iconAnchor: [25, 25],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [25, 25]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-flight-map',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    LeafletModule,
+    NgbModule
   ],
   templateUrl: './flight-map.component.html',
   styleUrl: './flight-map.component.scss'
 })
-export class FlightMapComponent implements OnInit{
+export class FlightMapComponent implements OnInit, AfterViewInit {
 
   flights: Flight[] = []
 
   isRunning = false;
+
+  private map: L.Map | null = null;
+
+  private markers: L.Marker<any>[] = []
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [49.499267623513205, 11.076298877163023],
+      zoom: 8
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
 
   constructor(private flightDataService: FlightDataClientService) {
   }
@@ -25,17 +64,23 @@ export class FlightMapComponent implements OnInit{
     this.scheduleFlightCall()
   }
 
+  ngAfterViewInit() {
+    this.initMap()
+  }
+
   scheduleFlightCall() {
     this.isRunning = true;
 
     setTimeout(() => {
-      console.log('Calling service method...');
-      this.flightDataService.getFlights().subscribe(data => this.flights = data);
+      this.flightDataService.getFlights().subscribe(data => {
+        this.flights = data;
+        this.addFlightsToMap()
+      });
 
       if (this.isRunning) {
         this.scheduleFlightCall();
       }
-    }, 1000);
+    }, 3500);
   }
 
   toogleIsRunning() {
@@ -43,5 +88,35 @@ export class FlightMapComponent implements OnInit{
     if (this.isRunning) {
       this.scheduleFlightCall()
     }
+  }
+
+  addFlightsToMap() {
+    for (let marker of this.markers) {
+      this.map!.removeLayer(marker);
+    }
+
+    for (let flight of this.flights) {
+      const marker = L.marker([flight.latitude, flight.longitude]);
+      const tooltip = this.createFlightTooltip(flight);
+      marker.bindTooltip(tooltip);
+      marker.bindPopup(this.createFlightPopup(flight), {
+        keepInView: true,
+        autoClose: false,
+        autoPan: false
+      })
+      this.markers.push(marker);
+      marker.addTo(this.map!)
+    }
+  }
+
+  createFlightTooltip(flight: Flight): string {
+    return '<div>' + flight.call_sign + '</div>'
+  }
+
+  createFlightPopup(flight: Flight): string {
+    return '<div> Callsign: ' + flight.call_sign + '</div>' +
+      '<div> Altitude: ' + flight.altitude + 'ft</div>' +
+      '<div> Squawk: ' + flight.squawk + '</div>' +
+      '<div><a href="http://www.flightradar24.com/' + flight.call_sign + '/" target="_blank" rel="noreferrer" class="external">Show on FlightRadar24</a></div>'
   }
 }
